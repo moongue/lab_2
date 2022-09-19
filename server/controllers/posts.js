d129c75cb9473eef96200ccdbd546f899435e64b
moongue <sasha.mojseenko2001@gmail.com>
2022-09-19 19:23:47 +0300

    feat: complete lab_2

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -5,7 +5,7 @@ import PostMessage from '../models/postMessage.js';
 
 const router = express.Router();
 
-export const getPosts = async (req, res) => { 
+export const getPosts = async (req, res) => {
     try {
         const postMessages = await PostMessage.find();
                 
--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -8,14 +7,14 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use('/api/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
-const PORT = process.env.PORT|| 5000;
+const CONNECTION_URL = 'mongodb+srv://moongue:1111@cluster0.2odyrie.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
